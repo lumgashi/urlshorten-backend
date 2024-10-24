36cea7d1f52d73caf38b2d3e32d0d8300dd8c2a4
lumgashi <lumgashi97@hotmail.com>
2024-10-24 13:33:21 +0200

    installed pino logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { PrismaModule } from './prisma/prisma.module';
 import { UrlModule } from './url/url.module';
 import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
   imports: [
@@ -20,6 +21,17 @@ import { APP_GUARD } from '@nestjs/core';
         limit: 10,
       },
     ]),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        customProps: (req, res) => ({
+          context: 'HTTP',
+        }),
+        transport: {
+          target: 'pino-pretty',
+        },
+      },
+    }),
   ],
   controllers: [AppController],
   providers: [
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.use(helmet());
+  app.useLogger(app.get(Logger));
   await app.listen(5000);
 }
 bootstrap();
